b1e846958b499e0d747dd5d55ccadffb9d57c770
Adam Wyłuda <adam.wyluda@gmail.com>
2016-05-27 12:46:54 +0200

    Basic Seq benchmark

--- benchmark/src/main/scala/Benchmarks.scala
+++ benchmark/src/main/scala/Benchmarks.scala
@@ -1,9 +1,60 @@
 package benchmark
 
 import org.openjdk.jmh.annotations._
-
 import offheap.collection._
+import org.openjdk.jmh.infra.Blackhole
 
-class Benchmarks {
+import scala.util.Random
+import scala.{collection => stdlib}
 
+@State(Scope.Thread)
+class StdlibSeqBenchmark {
+
+  val seq: stdlib.Seq[Int] = {
+    var seq = stdlib.Seq[Int]()
+    1 to 10000 foreach (i => seq = seq :+ i)
+    seq
+  }
+
+  val random = Random
+
+  @Benchmark
+  def readSequential(blackhole: Blackhole) = {
+    var i = 0
+    while (i < seq.size) {
+      blackhole.consume(seq(i))
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def readRandom(blackhole: Blackhole) = {
+    blackhole.consume(seq(random.nextInt(seq.size)))
+  }
+}
+
+@State(Scope.Thread)
+class SeqBenchmark {
+
+  val seq: Seq[Int] = {
+    val seq = new Seq[Int]
+    1 to 10000 foreach (seq.append(_))
+    seq
+  }
+
+  val random = Random
+
+  @Benchmark
+  def readSequential(blackhole: Blackhole) = {
+    var i = 0
+    while (i < seq.size) {
+      blackhole.consume(seq(i))
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def readRandom(blackhole: Blackhole) = {
+    blackhole.consume(seq(random.nextInt(seq.size)))
+  }
 }
