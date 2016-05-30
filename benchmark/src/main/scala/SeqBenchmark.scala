ac9b2dcfc0233a11c5084b58bd1762478fe6df17
Adam Wyłuda <adam.wyluda@gmail.com>
2016-05-30 10:16:08 +0200

    Reorganize SeqBenchmark

---
+++ benchmark/src/main/scala/SeqBenchmark.scala
@@ -0,0 +1,159 @@
+package benchmark
+
+import org.openjdk.jmh.annotations._
+import offheap.collection._
+import org.openjdk.jmh.infra.Blackhole
+
+import scala.util.Random
+import scala.{collection => stdlib}
+
+@State(Scope.Thread)
+class SeqBenchmark {
+
+  val seq: Seq[Int] = {
+    val seq = new Seq[Int]
+    1 to 10000 foreach (seq.append(_))
+    seq
+  }
+
+  val stdSeq: stdlib.mutable.ArrayBuffer[Int] = {
+    val seq = stdlib.mutable.ArrayBuffer[Int]()
+    1 to 10000 foreach (seq.append(_))
+    seq
+  }
+
+  val random = Random
+
+  @Benchmark
+  def readSequential(blackhole: Blackhole) = {
+    var i = 0
+    val size = seq.size
+    while (i < size) {
+      blackhole.consume(seq(i))
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def readSequentialStdlib(blackhole: Blackhole) = {
+    var i = 0
+    val size = stdSeq.size
+    while (i < size) {
+      blackhole.consume(stdSeq(i))
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def readRandom(blackhole: Blackhole) = {
+    blackhole.consume(seq(random.nextInt(seq.size)))
+  }
+
+  @Benchmark
+  def readRandomStdlib(blackhole: Blackhole) = {
+    blackhole.consume(stdSeq(random.nextInt(stdSeq.size)))
+  }
+
+  @Benchmark
+  def append() = {
+    val s = new Seq[Int](initialSize = 16)
+    var i = 0
+    while (i < 100000) {
+      s.append(i)
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def appendStdlib() = {
+    val s = stdlib.mutable.ArrayBuffer[Int]()
+    var i = 0
+    while (i < 100000) {
+      s.append(i)
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def updateSequential() = {
+    val s = seq
+    val size = s.size
+    var i = 0
+    while (i < size) {
+      s(i) = i * 2
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def updateSequentialStdlib() = {
+    val s = stdSeq
+    val size = s.size
+    var i = 0
+    while (i < size) {
+      s(i) = i * 2
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def updateRandom() = {
+    val s = seq
+    val size = s.size
+    var i = 0
+    while (i < size) {
+      s(random.nextInt(size)) = i * 2
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def updateRandomStdlib() = {
+    val s = stdSeq
+    val size = s.size
+    var i = 0
+    while (i < size) {
+      s(random.nextInt(size)) = i * 2
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def foreach(blackhole: Blackhole) = seq foreach (blackhole.consume(_))
+
+  @Benchmark
+  def foreachStdlib(blackhole: Blackhole) = stdSeq foreach (blackhole.consume(_))
+
+  @Benchmark
+  def remove() = {
+    val s = new Seq[Int]
+    s.append(seq)
+    while (s.nonEmpty) s.remove(0)
+  }
+
+  @Benchmark
+  def removeStdlib() = {
+    val s = stdlib.mutable.ArrayBuffer[Int](stdSeq: _*)
+    while (s.nonEmpty) s.remove(0)
+  }
+
+  @Benchmark
+  def prepend() = {
+    val s = new Seq[Int]
+    var i = 0
+    while (i < 10000) {
+      s.insert(0, i)
+      i += 1
+    }
+  }
+
+  @Benchmark
+  def prependStdlib() = {
+    val s = stdlib.mutable.ArrayBuffer[Int]()
+    var i = 0
+    while (i < 10000) {
+      s.insert(0, i)
+      i += 1
+    }
+  }
+}
