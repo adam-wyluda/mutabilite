2f498621371bf74ee671b65275007c6e12cbae72
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-01 16:44:33 +0200

    Opt-imization

--- core/src/main/scala/Opt.scala
+++ core/src/main/scala/Opt.scala
@@ -2,20 +2,20 @@ package offheap.collection
 
 class Opt[A] extends Traversable[A] {
 
-  private[this] class Holder(val elem: A)
-
-  private[this] var holder: Holder = null
+  private[this] var value: A = _
+  private[this] var empty: Boolean = true
 
   def this(elem: A) = {
     this()
-    holder = new Holder(elem)
+    this.value = elem
+    this.empty = false
   }
 
   // Dereference primitive or cast self to data class
   def get(): A =
-    if (nonEmpty) holder.elem else throw new NoSuchElementException
+    if (nonEmpty) value else throw new NoSuchElementException
 
-  override def isEmpty: Boolean = holder == null
+  override def isEmpty: Boolean = empty
   override def size: Int = if (nonEmpty) 1 else 0
   override def foreach[U](f: (A) => U): Unit = if (nonEmpty) f(get())
 }
