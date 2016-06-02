3f8eef4650e18062c6b0217b4dcde91eea6d16f1
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-02 19:08:59 +0200

    Some work on HashMap

--- core/src/main/scala/HashMap.scala
+++ core/src/main/scala/HashMap.scala
@@ -1,7 +1,15 @@
 package offheap.collection
 
-class HashMap[K, V]
-  extends Map[K, V] {
+// Uses ideas from:
+// https://doc.rust-lang.org/std/collections/struct.HashMap.html
+// http://sebastiansylvan.com/post/robin-hood-hashing-should-be-your-default-hash-table-implementation/
+// http://codecapsule.com/2013/11/11/robin-hood-hashing/
+class HashMap[K, V](initialSize: Int = 16) extends Map[K, V] {
+
+  private[this] val hashes: Array[Int] = new Array[Int](initialSize)
+  private[this] val _keys: Array[K] = new Array[K](initialSize)
+  private[this] val _values: Array[V] = new Array[V](initialSize)
+  private[this] var _size = 0
 
   def apply(key: K): Opt[V] = ???
 
@@ -13,13 +21,22 @@ class HashMap[K, V]
 
   def values: NaiveSeq[V] = ???
 
-  def contains(key: K): Boolean = ???
+  def contains(key: K): Boolean = this(key).nonEmpty
 
-  private def keyIndex(key: K) = ???
+  private[this] def capacity = _keys.size
+  private[this] def mask = _keys.size - 1
 
-  override def isEmpty = ???
+  private[this] def init(pos: Int, hash: Int, key: K, value: V) = {
+    hashes(pos) = hash
+    _keys(pos) = key
+    _values(pos) = value
+  }
 
-  override def size: Int = ???
+
+
+  override def isEmpty = _size == 0
+
+  override def size: Int = _size
 
   override def foreach[U](f: ((K, V)) => U): Unit = ???
 }
