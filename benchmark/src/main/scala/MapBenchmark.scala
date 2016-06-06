77d682cba63b497c8564a8bc6aa0f2741d273dae
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-06 14:31:27 +0200

    Custom key class instead of string in benchmark

--- benchmark/src/main/scala/MapBenchmark.scala
+++ benchmark/src/main/scala/MapBenchmark.scala
@@ -7,37 +7,61 @@ import org.openjdk.jmh.infra.Blackhole
 import scala.util.Random
 import scala.collection.mutable.{OpenHashMap => StdlibMap}
 
-@State(Scope.Thread)
-class MapBenchmark {
+object MapBenchmark {
 
-  val mapSize = 100000
+  val mapSize = 1000000
   val random = Random
 
-  val keys: Array[String] = {
-    val keys = new Array[String](mapSize)
-    0 until mapSize foreach (keys(_) = random.nextString(10))
+  class Key(val x: Int, val y: Int, val z: Int) {
+    override def equals(that: Any) = {
+      val t = that.asInstanceOf[Key]
+      x == t.x && y == t.y && z == t.z
+    }
+    override def hashCode = (x * 31 + y) * 31 + z
+  }
+
+  object Key {
+    @inline
+    def generate = new Key(random.nextInt(), random.nextInt(), random.nextInt())
+  }
+
+  val keys: Array[Key] = {
+    val keys = new Array[Key](mapSize)
+    0 until mapSize foreach (keys(_) = Key.generate)
     keys
   }
 
-  val map: HashMap[String, Int] = {
-    val map = new HashMap[String, Int]()
+  val initialMapSize = {
+    var size = 1
+    while (size < mapSize) size *= 2
+    size
+  }
+}
+
+@State(Scope.Thread)
+class MapBenchmark {
+
+  import MapBenchmark._
+
+  val map: HashMap[Key, Int] = {
+    val map = new HashMap[Key, Int](initialSize = initialMapSize)
     0 until mapSize foreach (i => map.put(keys(i), i * i))
     map
   }
 
-  val stdMap: StdlibMap[String, Int] = {
-    val map = StdlibMap[String, Int]()
+  val stdMap: StdlibMap[Key, Int] = {
+    val map = new StdlibMap[Key, Int](initialSize = initialMapSize)
     0 until mapSize foreach (i => map.put(keys(i), i * i))
     map
   }
 
-  var randKey: String = _
-  var nonexistingKey: String = _
+  var randKey: Key = _
+  var nonExistingKey: Key = _
 
   @Setup(Level.Invocation)
   def setup = {
     randKey = keys(random.nextInt(mapSize))
-    nonexistingKey = random.nextString(11)
+    nonExistingKey = Key.generate
   }
 
   @Benchmark
@@ -47,14 +71,14 @@ class MapBenchmark {
   def getStdlib = stdMap.get(randKey)
 
   @Benchmark
-  def getNonExisting = map(nonexistingKey)
+  def getNonExisting = map(nonExistingKey)
 
   @Benchmark
-  def getNonExistingStdlib = stdMap.get(nonexistingKey)
+  def getNonExistingStdlib = stdMap.get(nonExistingKey)
 
   @Benchmark
   def put = {
-    val m = new HashMap[String, Int]
+    val m = new HashMap[Key, Int](initialSize = initialMapSize)
     var i = 0
     while (i < mapSize) {
       m.put(keys(i), i)
@@ -64,7 +88,7 @@ class MapBenchmark {
 
   @Benchmark
   def putStdlib = {
-    val m = StdlibMap[String, Int]()
+    val m = new StdlibMap[Key, Int](initialSize = initialMapSize)
     var i = 0
     while (i < mapSize) {
       m.put(keys(i), i)
@@ -81,69 +105,63 @@ class MapBenchmark {
 
   @Benchmark
   def putRemoveRead(blackhole: Blackhole) = {
-    val map = new HashMap[String, Int]()
+    val map = new HashMap[Key, Int](initialSize = initialMapSize)
     var i = 0
-    while (i < 10000) { map.put(keys(i), i); i += 1 }
+    while (i < mapSize) { map.put(keys(i), i); i += 1 }
     i = 0
-    while (i < 1000) { map.remove(keys(i * 10)); i += 1 }
+    while (i < mapSize / 10) { map.remove(keys(i * 10)); i += 1 }
     i = 0
-    while (i < 10000) { blackhole.consume(map(keys(i))); i += 1 }
+    while (i < mapSize) { blackhole.consume(map(keys(i))); i += 1 }
   }
 
   @Benchmark
   def putRemoveReadStdlib(blackhole: Blackhole) = {
-    val map = new StdlibMap[String, Int]()
+    val map = new StdlibMap[Key, Int](initialSize = initialMapSize)
     var i = 0
-    while (i < 10000) { map.put(keys(i), i); i += 1 }
+    while (i < mapSize) { map.put(keys(i), i); i += 1 }
     i = 0
-    while (i < 1000) { map.remove(keys(i * 10)); i += 1 }
+    while (i < mapSize / 10) { map.remove(keys(i * 10)); i += 1 }
     i = 0
-    while (i < 10000) { blackhole.consume(map.get(keys(i))); i += 1 }
+    while (i < mapSize) { blackhole.consume(map.get(keys(i))); i += 1 }
   }
 }
 
 @State(Scope.Thread)
 class MapRemoveBenchmark {
 
-  var map: HashMap[String, Int] = _
-  val keys: Array[String] = {
-    val keys = new Array[String](1000)
-    0 until 1000 foreach (i => keys(i) = i toString)
-    keys
-  }
+  import MapBenchmark._
+
+  var map: HashMap[Key, Int] = _
 
   @Setup(Level.Invocation)
   def setup = {
-    map = new HashMap[String, Int]
-    0 until 1000 foreach (i => map.put(keys(i), i * i))
+    map = new HashMap[Key, Int]
+    0 until mapSize foreach (i => map.put(keys(i), i * i))
   }
 
   @Benchmark
   def benchmark = {
     var i = 0
-    while (i < 1000) { map.remove(keys(i)); i += 1 }
+    while (i < mapSize / 10) { map.remove(keys(i * 10)); i += 1 }
   }
 }
 
 @State(Scope.Thread)
 class MapRemoveStdlibBenchmark {
 
-  var map: StdlibMap[String, Int] = _
-  val keys: Array[String] = {
-    val keys = new Array[String](1000)
-    0 until 1000 foreach (i => keys(i) = i toString)
-    keys
-  }
+  import MapBenchmark._
+
+  var map: StdlibMap[Key, Int] = _
 
   @Setup(Level.Invocation)
   def setup = {
-    map = StdlibMap[String, Int]()
-    0 until 1000 foreach (i => map.put(keys(i), i * i))
+    map = StdlibMap[Key, Int]()
+    0 until mapSize foreach (i => map.put(keys(i), i * i))
   }
 
   @Benchmark
   def benchmark = {
     var i = 0
-    while (i < 1000) { map.remove(keys(i)); i += 1 }
+    while (i < mapSize / 10) { map.remove(keys(i * 10)); i += 1 }
   }
 }
