e988e0ee2bfff6acc87330ab124565ec30783d89
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-22 16:18:37 +0200

    Move specialized HashEq and Opt to api module

--- run_gyb.sh
+++ run_gyb.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-python project/gyb.py ./specialized/src/main/resources/gyb/BufferSeq.scala.gyb > ./specialized/src/main/scala/codegen/GenBufferSeq.scala
-python project/gyb.py ./specialized/src/main/resources/gyb/HashMap.scala.gyb > ./specialized/src/main/scala/codegen/GenHashMap.scala
-python project/gyb.py ./specialized/src/main/resources/gyb/HashSet.scala.gyb > ./specialized/src/main/scala/codegen/GenHashSet.scala
-python project/gyb.py ./specialized/src/main/resources/gyb/HashEq.scala.gyb > ./specialized/src/main/scala/codegen/GenHashEq.scala
-python project/gyb.py ./specialized/src/main/resources/gyb/Opt.scala.gyb > ./specialized/src/main/scala/codegen/GenOpt.scala
+mkdir -p ./api/src/main/scala/codegen
+python project/gyb.py ./api/src/main/resources/gyb/HashEq.scala.gyb > ./api/src/main/scala/codegen/SpecializedHashEq.scala
+python project/gyb.py ./api/src/main/resources/gyb/Opt.scala.gyb    > ./api/src/main/scala/codegen/SpecializedOpt.scala
+
+mkdir -p ./specialized/src/main/scala/codegen
+python project/gyb.py ./specialized/src/main/resources/gyb/BufferSeq.scala.gyb > ./specialized/src/main/scala/codegen/SpecializedBufferSeq.scala
+python project/gyb.py ./specialized/src/main/resources/gyb/HashMap.scala.gyb   > ./specialized/src/main/scala/codegen/SpecializedHashMap.scala
+python project/gyb.py ./specialized/src/main/resources/gyb/HashSet.scala.gyb   > ./specialized/src/main/scala/codegen/SpecializedHashSet.scala
