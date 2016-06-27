e0f15bb9b1eed1c197283ca4ed929cdb61f0579f
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-27 16:45:25 +0200

    Workaround for data class issue

--- run_gyb.sh
+++ run_gyb.sh
@@ -9,7 +9,11 @@ python project/gyb.py ./specialized/src/main/resources/gyb/BufferSeq.scala.gyb >
 python project/gyb.py ./specialized/src/main/resources/gyb/HashMap.scala.gyb   > ./specialized/src/main/scala/codegen/SpecializedHashMap.scala
 python project/gyb.py ./specialized/src/main/resources/gyb/HashSet.scala.gyb   > ./specialized/src/main/scala/codegen/SpecializedHashSet.scala
 
-mkdir -p ./offheap/src/main/scala/codegen
-python project/gyb.py ./offheap/src/main/resources/gyb/BufferSeq.scala.gyb > ./offheap/src/main/scala/codegen/OffheapBufferSeq.scala
-python project/gyb.py ./offheap/src/main/resources/gyb/HashMap.scala.gyb   > ./offheap/src/main/scala/codegen/OffheapHashMap.scala
-python project/gyb.py ./offheap/src/main/resources/gyb/HashSet.scala.gyb   > ./offheap/src/main/scala/codegen/OffheapHashSet.scala
+# mkdir -p ./offheap/src/main/scala/codegen
+python project/gyb.py ./offheap/src/main/resources/gyb/BufferSeq.scala.gyb > ./offheap/src/test/scala/AnOffheapBufferSeq.scala
+python project/gyb.py ./offheap/src/main/resources/gyb/HashMap.scala.gyb   > ./offheap/src/test/scala/AnOffheapHashMap.scala
+python project/gyb.py ./offheap/src/main/resources/gyb/HashSet.scala.gyb   > ./offheap/src/test/scala/AnOffheapHashSet.scala
+
+python project/gyb.py ./offheap/src/main/resources/gyb/BufferSeq.scala.gyb > ./benchmark/src/main/scala/AnOffheapBufferSeq.scala
+python project/gyb.py ./offheap/src/main/resources/gyb/HashMap.scala.gyb   > ./benchmark/src/main/scala/AnOffheapHashMap.scala
+python project/gyb.py ./offheap/src/main/resources/gyb/HashSet.scala.gyb   > ./benchmark/src/main/scala/AnOffheapHashSet.scala
