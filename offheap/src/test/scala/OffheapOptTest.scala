278ab40a360b6e5ee4294d8449f2fe79e7c3f9ea
Adam Wyłuda <adam.wyluda@gmail.com>
2016-06-29 15:13:37 +0200

    Offheap Opt test

--- offheap/src/test/scala/OffheapOptTest.scala
+++ offheap/src/test/scala/OffheapOptTest.scala
@@ -3,11 +3,32 @@ package test
 import offheap.collection._
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
-import HashEq.Implicits._
+class OffheapOptTest extends FunSuite with BeforeAndAfter {
+  implicit val alloc = scala.offheap.malloc
 
-class OffheapOptTest extends FunSuite with BeforeAndAfter {//with OptTest {
-implicit val alloc = scala.offheap.malloc
+  val optInt = OffheapOpt_Int(25)
+  val noneInt = OffheapOpt_Int.empty
 
-  def provideOpt_Int(value: Int): Opt_Int = OffheapOpt_Int(value)
-  def provideNone: Opt_Int = OffheapOpt_Int.empty
+  test("isEmpty") {
+    assert(optInt.notEmpty)
+    assert(noneInt.empty)
+  }
+
+  test("get") {
+    assert(optInt.get == 25)
+    intercept[NullPointerException] {
+      noneInt.get
+    }
+  }
+
+  test("foreach") {
+    var optSum = 0
+    var noneSum = 0
+
+    optInt.foreach(optSum += _)
+    noneInt.foreach(noneSum += _)
+
+    assert(optSum == 25)
+    assert(noneSum == 0)
+  }
 }
