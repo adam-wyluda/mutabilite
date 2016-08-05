62a02795ddd4a7bc54a9f7f8a437a91bd7fcc459
Adam Wyłuda <adam.wyluda@gmail.com>
2016-08-05 11:24:09 +0200

    Move GYB scripts from resources

--- run_gyb.sh
+++ run_gyb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p ./core/src/main/scala/codegen
-python project/gyb.py ./core/src/main/resources/gyb/HashEq.scala.gyb    > ./core/src/main/scala/codegen/SpecializedHashEq.scala
-python project/gyb.py ./core/src/main/resources/gyb/BufferSeq.scala.gyb > ./core/src/main/scala/codegen/SpecializedBufferSeq.scala
-python project/gyb.py ./core/src/main/resources/gyb/HashMap.scala.gyb   > ./core/src/main/scala/codegen/SpecializedHashMap.scala
-python project/gyb.py ./core/src/main/resources/gyb/HashSet.scala.gyb   > ./core/src/main/scala/codegen/SpecializedHashSet.scala
+python project/gyb.py ./core/src/main/gyb/HashEq.scala.gyb    > ./core/src/main/scala/codegen/SpecializedHashEq.scala
+python project/gyb.py ./core/src/main/gyb/BufferSeq.scala.gyb > ./core/src/main/scala/codegen/SpecializedBufferSeq.scala
+python project/gyb.py ./core/src/main/gyb/HashMap.scala.gyb   > ./core/src/main/scala/codegen/SpecializedHashMap.scala
+python project/gyb.py ./core/src/main/gyb/HashSet.scala.gyb   > ./core/src/main/scala/codegen/SpecializedHashSet.scala
